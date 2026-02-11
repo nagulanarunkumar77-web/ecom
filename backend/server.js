1440c847e07d0e9ad10332baaaa2f4654f956a8b
nagulanarunkumar77-web <nagulanarunkumar77@gmail.com>
2026-02-12 00:59:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use((req, res) => {
 
 // Database
 mongoose
-  .connect(process.env.MONGO_UR
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
